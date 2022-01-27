21c9179caa00a52e417b735ba09f5929574a99e7
nektovova <69082558+nektovova@users.noreply.github.com>
2022-01-27 12:16:29 +0300

    Create stat.py

---
+++ stat.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="123456",
+  database="bots_users"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM Users")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
