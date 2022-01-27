d1036ddae1370d04f71da83feaa8f1178b150a5c
nektovova <69082558+nektovova@users.noreply.github.com>
2022-01-27 12:34:40 +0300

    Update stat.py

--- stat.py
+++ stat.py
@@ -16,5 +16,16 @@ myresult = mycursor.fetchall()
 for x in myresult:
   print(x)
 
+#считаем все строки
 count_selected = mycursor.rowcount
 print("total rows: ",count_selected)
+
+#сколько start у бота spytwibot
+spytwibot_cursor = mydb.cursor()
+spytwibot_cursor.execute("SELECT from Users where botname='spytwibot' and action='start'")
+spytwibot_cursor_result = spytwibot_cursor.fetchall()
+print("spytwibot start action: ",spytwibot_cursor_result)
+
+
+
+
